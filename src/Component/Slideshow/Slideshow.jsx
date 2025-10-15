88a1c27ba2922016ff2fcc66beb844e9a90c9a5d
ValKer-OPCS <icyfry@gmail.com>
2025-10-15 11:01:17 +0200

    feat : Add Slideshow component with navigation arrows
    Introduces a new Slideshow component with left/right arrow SVG assets and associated SCSS styles. The component supports image navigation and displays a counter when multiple images are present.

---
+++ src/Component/Slideshow/Slideshow.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import LeftArrow from "../../Assets/arrowLeft.svg";
+import RightArrow from "../../Assets/arrowRight.svg";
+import styles from "./styles.module.scss";
+
+const Slideshow = ({ pictures, title }) => {
+
+
+    
+    const [currentImage, setCurrentImage] = useState(0);
+
+    const showNextImage = () => {
+        setCurrentImage(currentImage === pictures.length - 1 ? 0 : currentImage + 1);
+    };
+
+    const showPreviousImage = () => {
+        setCurrentImage(currentImage === 0 ? pictures.length - 1 : currentImage - 1);
+    };
+
+    const hasMultipleImages = pictures.length > 1;
+
+    return (
+        <div className={styles.slideshow}>
+            <div className={styles.imageWrapper}>
+                <img src={pictures[currentImage]} alt={title} />
+            </div>
+
+            {hasMultipleImages && (
+                <>
+                    <span onClick={showPreviousImage} className={styles.arrowLeft}>
+                        <img src={LeftArrow} alt="Précédent" />
+                    </span>
+                    <span onClick={showNextImage} className={styles.arrowRight}>
+                        <img src={RightArrow} alt="Suivant" />
+                    </span>
+
+                    <p className={styles.counter}>
+                        {currentImage + 1} / {pictures.length}
+                    </p>
+                </>
+            )}
+        </div>
+    );
+};
+
+export default Slideshow;
