07d010ea6442824f1a5529128b8da49fe0cdbff2
ValKer-OPCS <icyfry@gmail.com>
2025-10-15 11:01:29 +0200

    feat : Add rental data fetching and slideshow to Rental page
    Introduces logic to fetch rental data by ID if not provided via location state, displays a loader while fetching, and integrates the Slideshow component to show rental pictures and title.

--- src/Pages/Rental/Rental.jsx
+++ src/Pages/Rental/Rental.jsx
@@ -1,13 +1,37 @@
 /* import styles from './styles.module.scss' */
+import Slideshow from '../../Component/Slideshow/Slideshow.jsx'
+import { useParams , useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Loader from '../../Component/Loader/Loader.jsx';
 
 
 
 const Rental = () => {
+  const { id } = useParams();
+  const location = useLocation();
+  const [rental, setRental] = useState(location.state?.rental || null);
+
+  useEffect(() => {
+    if (!rental) {
+      fetch("/logements.json")
+        .then((res) => res.json())
+        .then((data) => {
+          const found = data.find((item) => item.id === id);
+          setRental(found);
+        })
+        .catch((err) => console.error(err));
+    }
+  }, [id, rental]);
+
+  if (!rental) {
+    return <Loader />;
+  }
+
   return (
     <div>
-      
+      <Slideshow pictures={rental.pictures} title={rental.title} />
     </div>
-  )
-};
+  );
+}
 
-export default Rental
+export default Rental;
