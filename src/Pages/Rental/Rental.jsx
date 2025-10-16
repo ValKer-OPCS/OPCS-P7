3764841ca327d668607e95888db58bc1fb74f3cc
ValKer-OPCS <icyfry@gmail.com>
2025-10-16 09:21:19 +0200

    feat : Redirect to error page if rental not found
    Adds navigation to the '/Error' route when a rental with the specified ID is not found in the fetched data. This improves user experience by handling invalid rental IDs gracefully.

--- src/Pages/Rental/Rental.jsx
+++ src/Pages/Rental/Rental.jsx
@@ -1,6 +1,6 @@
 /* import styles from './styles.module.scss' */
 import Slideshow from '../../Component/Slideshow/Slideshow.jsx'
-import { useParams , useLocation } from "react-router-dom";
+import { useParams , useLocation , useNavigate  } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Loader from '../../Component/Loader/Loader.jsx';
 
@@ -9,6 +9,7 @@ import Loader from '../../Component/Loader/Loader.jsx';
 const Rental = () => {
   const { id } = useParams();
   const location = useLocation();
+  const navigate = useNavigate();
   const [rental, setRental] = useState(location.state?.rental || null);
 
   useEffect(() => {
@@ -17,11 +18,14 @@ const Rental = () => {
         .then((res) => res.json())
         .then((data) => {
           const found = data.find((item) => item.id === id);
+          if (!found) {
+            navigate("/Error", { replace: true });
+            return;}
           setRental(found);
         })
         .catch((err) => console.error(err));
     }
-  }, [id, rental]);
+  }, [id, rental, navigate]);
 
   if (!rental) {
     return <Loader />;
